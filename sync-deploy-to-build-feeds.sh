e4aa74f9b1c4f4ec6d56205a3977b62fc3b66efe
mpvader <mvader@victronenergy.com>
2016-05-10 07:27:12 +0000

    add sync-deploy-to-build-feeds.sh, and use it in all ci jobs

---
+++ sync-deploy-to-build-feeds.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#CI_BUILD_ID
+
+
+echo "Synchronizing build feed"
+# Strip https://git.victronenergy.com/, add repo-name, branch, hash, job-name
+server_path=$(echo "$CI_BUILD_REPO" | sed 's#^[^:]\+://[^/]\+/##; s#/$##')/$CI_BUILD_REF_NAME/$(echo $CI_BUILD_REF | cut -c1-8)/$CI_BUILD_NAME
+# TODO: re-enable this check
+#if ! [[ "$server_path" =~ ^[^/]+/[^/]+/[^/]+$ ]]; then
+#	>&2 echo "Invalid server path $server_path extracted from CI_BUILD_REPO=$CI_BUILD_REPO CI_BUILD_REF_NAME=$CI_BUILD_REF_NAME CI_BUILD_ID=$CI_BUILD_ID"
+#	exit 1
+#fi
+if [[ "/${server_path}/" == */./* ]] || [[ "/${server_path}/" == */../* ]]
+then
+	>&2 echo "Insecure path $server_path"
+	exit 2
+fi
+HOST="build-feeds.victronenergy.com"
+FULL_PATH="/var/www/html/build-feeds/$server_path"
+echo "Uploading to: $HOST:$FULL_PATH"
+ssh builder@$HOST "mkdir -p $FULL_PATH/"
+rsync -v -rlt --delete ./deploy builder@$HOST:$FULL_PATH
+echo ""
+echo "See https://$HOST/$server_path/ for the results."
+
