347bdc7c981c4d568bf02aac0660a6e60b806171
Jeroen Hofstee <jhofstee@victronenergy.com>
2016-07-06 13:32:53 +0200

    provide several variables for repo

--- git-fetch-remote.sh
+++ git-fetch-remote.sh
@@ -3,8 +3,35 @@
 printf "\n--------------- $1 - $2 -------------\n"
 
 if [ $# -lt 4 ]; then
-	echo "$0 directory fetch-url push-url (upstream|-) (checkout-branch|-) (upstream-branch|-)"
+	echo "$0 directory fetch-url push-url (upstream-url|-) (checkout-branch|-) (upstream-branch|-)"
 	exit 1
+fi
+
+prop=build/prop.`basename $1`
+
+# fetch url / push url might differ, commonly fetch by https, push by ssh
+echo "export fetch_url=$2" > $prop
+echo "export git_repo=${2##*/}" > $prop
+echo "export push_url=$3" >> $prop
+
+if [ "$4" != "-" ]; then
+	echo "export upstream_url=$4" >> $prop
+else
+	echo "export upstream_url=" >> $prop
+fi
+
+if [ "$5" != "-" ]; then
+	echo "export checkout_branch=$5" >> $prop
+else
+	echo "export checkout_branch=" >> $prop
+fi
+
+if [ "$6" != "-" ]; then
+	echo "export upstream_full_branch=$6" >> $prop
+	echo "export upstream_branch=${6##*/}" >> $prop
+else
+	echo "export upstream_full_branch=" >> $prop
+	echo "export upstream_branch=" >> $prop
 fi
 
 git clone --no-checkout $2 $1
