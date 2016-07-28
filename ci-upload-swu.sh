19cb70e3526110454657c8feec38de5ab2aad686
Jeroen Hofstee <jhofstee@victronenergy.com>
2016-07-28 09:35:37 +0200

    add script to upload swu file

---
+++ ci-upload-swu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+deploy="deploy/venus/images"
+image="bpp3-rootfs"
+machine="ccgx"
+remote="victron_www_swu@updates.victronenergy.com"
+
+swu=$(ls  $deploy/$machine/$image-swu-$machine-*.swu  | sort | tail -n1)
+if [ -z ${swu} ]; then echo no  swu  file found; exit 1; fi
+
+if [[ "$swu" =~ ^$deploy/$machine/$image-swu-$machine-(.*).swu$ ]];
+then
+	swu_build=${BASH_REMATCH[1]} ;
+else
+	echo "No wsu-build?";
+	exit 1
+fi
+
+rfile="venus-swu-$machine-$swu_build.swu"
+scp $swu $remote:$rfile
+ssh $remote ln -s $rfile "venus-swu-$machine.swu"
