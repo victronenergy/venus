f24ae320e01effb53be55fc2b6e24e093b729c97
Jeroen Hofstee <jhofstee@victronenergy.com>
2016-09-27 14:13:30 +0200

    build deb: stop on error

--- build-all-debian-packages.sh
+++ build-all-debian-packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ./build-debian-packages.sh raspbian-jessie
 ./build-debian-packages.sh raspbian-wheezy
 ./build-debian-packages.sh debian-jessie
--- build-debian-packages.sh
+++ build-debian-packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 case "$1" in
 raspbian-wheezy|raspbian-jessie|debian-jessie)
 	;;
