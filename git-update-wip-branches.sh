07f7a76b2751c78f22ed9ef3463eea03d5b01e4d
mpvader <mvader@victronenergy.com>
2016-09-29 15:47:09 +0200

    git-update-wip-branches: also work with older awk version

--- git-update-wip-branches.sh
+++ git-update-wip-branches.sh
@@ -7,7 +7,7 @@ if [ "$branch" != "$checkout_branch" ]; then
 	echo "branch changed $src $checkout_branch => $branch"
 	for conf in $(find ../../configs -name repos.conf); do
 		# change the branch to the current WIP branches, but only if the checkout-branches where equal in the first place
-		awk -i inplace -v src=$src -v checkout_branch=$checkout_branch -v branch=$branch '$1==src && $5==checkout_branch {$5=branch}1' $conf
+		awk -v src=$src -v checkout_branch=$checkout_branch -v branch=$branch '$1==src && $5==checkout_branch {$5=branch}1' $conf > tmp && mv tmp $conf
 	done
 fi
 
