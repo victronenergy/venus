fa65e291e15119197b09fe8764d79d18eed3d340
Jeroen Hofstee <jhofstee@victronenergy.com>
2016-10-24 16:52:24 +0200

    build-danny-ccgx: remove renaming the image
    now that the build switched to venus-image itself, there is no
    need to rename the image anymore.

--- build-danny-ccgx-swu.sh
+++ build-danny-ccgx-swu.sh
@@ -3,16 +3,3 @@
 . ./sources/openembedded-core/oe-init-build-env build sources/bitbake
 export MACHINE=ccgx
 bitbake venus-upgrade-image venus-install-sdcard
-
-# Rename the swu image to venus-swu-ccgx.....
-
-# first exit the ./build directory
-cd ..
-
-path=deploy/venus/images/ccgx
-old=$(realpath ${path}/bpp3-rootfs-swu-ccgx.swu)
-name=$(basename ${old})
-name=venus-swu-ccgx-${name:21}
-mv ${old} ${path}/${name}
-ln -sf ${name} ${path}/venus-swu-ccgx.swu
-rm ${path}/bpp3-rootfs-swu-ccgx.swu
