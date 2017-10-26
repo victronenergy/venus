2e3f1ad601152a0db24f1d682a65197aca552678
Jeroen Hofstee <jhofstee@victronenergy.com>
2017-10-26 21:35:13 +0200

    remove unused scripts

--- build-jethro-cortexa7hf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ./sources/openembedded-core/oe-init-build-env build sources/bitbake
-export MACHINE=raspberrypi2
-bitbake packagegroup-core-boot packagegroup-base packagegroup-venus-base packagegroup-ve-console-apps meta-toolchain-qte
-
--- build-jethro-cortexa8hf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ./sources/openembedded-core/oe-init-build-env build sources/bitbake
-export MACHINE=dummy-cortexa8hf
-bitbake packagegroup-core-boot packagegroup-base packagegroup-venus-base packagegroup-ve-console-apps meta-toolchain-qte
-
