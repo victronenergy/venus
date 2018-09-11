bb600e583804c0ef473cbe40a6167a66888d13b3
Jeroen Hofstee <jhofstee@victronenergy.com>
2018-09-11 12:34:49 +0200

    swu.sh: support updates over port forwards
    if localhost is the destionation assume it is a port forwarded connection.

--- swu.sh
+++ swu.sh
@@ -2,6 +2,10 @@
 
 here=$(dirname $0)
 host=$1
-machine=$(ssh root@$host cat /etc/venus/machine)
-cat $here/deploy/venus/images/$machine/venus-swu-$machine.swu | ssh root@$host /opt/victronenergy/swupdate-scripts/check-updates.sh -swu file:///dev/stdin
+if [ "$host" = "localhost" ]; then
+        sshargs="-p4000 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet"
+fi
+
+machine=$(ssh $sshargs root@$host cat /etc/venus/machine)
+cat $here/deploy/venus/images/$machine/venus-swu-$machine.swu | ssh $sshargs root@$host /opt/victronenergy/swupdate-scripts/check-updates.sh -swu file:///dev/stdin
 
