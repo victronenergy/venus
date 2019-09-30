b9ef25a297fae55c05a9603b21aa222190f0833b
Jeroen Hofstee <jhofstee@victronenergy.com>
2019-09-30 23:00:36 +0200

    allow parallel uploads
    Better be careful with this, since output is mixed etc.

--- swu.sh
+++ swu.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+set -e
+
 here=$(dirname $0)
-host=$1
-if [ "$host" = "localhost" ]; then
-        sshargs="-p4000 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet"
+
+update() {
+	host=$1
+	if [ "$host" = "localhost" ]; then
+		sshargs="-p4000 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet"
+	fi
+
+	machine=$(ssh $sshargs root@$host cat /etc/venus/machine)
+	cat $here/deploy/venus/images/$machine/venus-swu-$machine.swu | ssh $sshargs root@$host /opt/victronenergy/swupdate-scripts/check-updates.sh -swu file:///dev/stdin
+}
+
+if [ "$#" -eq 0 ]; then
+	echo "$0 host1 [host2 ...]"
+	echo
+	echo "Upload the latests build swu file to the host(s)."
+	echo
+	echo "Note: with multiple hosts the output is a complete mess."
+	exit
 fi
 
-machine=$(ssh $sshargs root@$host cat /etc/venus/machine)
-cat $here/deploy/venus/images/$machine/venus-swu-$machine.swu | ssh $sshargs root@$host /opt/victronenergy/swupdate-scripts/check-updates.sh -swu file:///dev/stdin
+while (( "$#" ));
+do
+	update $1 &
+	shift
+done
 
+wait
