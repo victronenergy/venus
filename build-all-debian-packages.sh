5f54ec60cb31c6f4a5cff9eb00b143cf747c08ec
Jeroen Hofstee <jhofstee@victronenergy.com>
2021-07-09 16:53:21 +0200

    remove support for jessie (EOL since June 2020)
    Since jessie is initially released in 2015 it lacks several python3 packages,
    instead, so drop it, since it is old and EOL.

--- build-all-debian-packages.sh
+++ build-all-debian-packages.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-./build-debian-packages.sh raspbian-jessie
-./build-debian-packages.sh debian-jessie
 ./build-debian-packages.sh raspbian-buster
 ./build-debian-packages.sh debian-buster
